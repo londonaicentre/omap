467acd77be1a8daa62f6ec2d5a0132e29ac93dc8
Stephanie Jones <stephaniejones@Stephanies-MacBook-Pro.local>
2025-03-11 09:44:56 +0000

    deleting debugging messages

--- conftest.py
+++ conftest.py
@@ -5,6 +5,3 @@ import os
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
 if src_path not in sys.path:
     sys.path.insert(0, src_path)
-
-print(f"DEBUG: conftest.py is running")
-print(f"DEBUG: sys.path = {sys.path}")
